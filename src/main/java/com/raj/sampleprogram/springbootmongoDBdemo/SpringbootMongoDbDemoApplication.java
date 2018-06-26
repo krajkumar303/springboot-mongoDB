c7174f09a6e1f64fd420b2f85b03e49a879dc08a
Rajkumar <krajkumar.me@gmail.com>
2018-06-26 07:35:58 -0400

    Added Default files

---
+++ src/main/java/com/raj/sampleprogram/springbootmongoDBdemo/SpringbootMongoDbDemoApplication.java
@@ -0,0 +1,12 @@
+package com.raj.sampleprogram.springbootmongoDBdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoDbDemoApplication.class, args);
+	}
+}
